609c959c611626a32d3bc4683c85422483a46500
MSpilari <mb.spilari@hotmail.com>
2024-12-02 12:33:53 -0300

    feat: add validation on tokenDTO

--- src/main/java/dev/mspilari/voucher_api/dto/TokenDto.java
+++ src/main/java/dev/mspilari/voucher_api/dto/TokenDto.java
@@ -1,5 +1,12 @@
 package dev.mspilari.voucher_api.dto;
 
-public record TokenDto(String token1, String token2, String token3, String token4, String token5) {
+import jakarta.validation.constraints.NotBlank;
+
+public record TokenDto(
+        @NotBlank String token1,
+        @NotBlank String token2,
+        @NotBlank String token3,
+        @NotBlank String token4,
+        @NotBlank String token5) {
 
 }
