a5e542011908009c70373d86e5a190a0e81e136f
MSpilari <mb.spilari@hotmail.com>
2025-03-12 12:01:16 -0300

    feat: create the TokenRedisRepository with saveToken method

---
+++ src/main/java/dev/mspilari/voucher_api/repositories/TokenRedisRepository.java
@@ -0,0 +1,20 @@
+package dev.mspilari.voucher_api.repositories;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TokenRedisRepository {
+
+    private RedisTemplate<String, String> redisTemplate;
+
+    public TokenRedisRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void saveToken(String token, String validity, long timeExpiration) {
+        redisTemplate.opsForValue().set(token, "Válido até: " + validity, timeExpiration, TimeUnit.SECONDS);
+    }
+}
